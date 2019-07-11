ee8f8424a608fdd84fed88ed23ba55153ee44b76
unknown <salvadoranr16@gmail.com>
2019-07-11 20:11:48 +0200

    last commit

---
+++ NVL-JS-P6a.js
@@ -0,0 +1,7 @@
+'use strict'
+var firstName= 'pepe';
+var lastName =' acosto';
+var age=24;
+var weekDays=['lunes','martes','meircoles','jueves','viernes','sabado','domingo',];
+var gravityValue= 9.81;
+var oddNumber =[1,3,5]
